f3f4b11f6dd8c3f0adfc4ee78b32d66e2a4ab991
AjithK14 <kemisettia@gmail.com>
2018-01-27 02:20:07 -0500

    The infamous A* implemented?

--- Ajith1_26_2018/run.py
+++ Ajith1_26_2018/run.py
@@ -286,7 +286,7 @@ def astar(unit, dest):
   return;
 
 def reconPath(cameFrom,minKey,start,unit):
-  print("MOVEMENT HEAT", unit.movement_heat())
+  print("MOVEMENT HEAT", unit.movement_heat());
   if unit.movement_heat() < 10:
     totalPath = [minKey]
     while minKey in cameFrom:
@@ -304,7 +304,7 @@ def reconPath(cameFrom,minKey,start,unit):
     else:
       if dx == 0: gc.move_robot(unit.id,bc.Direction.South); return
       elif dx ==1: gc.move_robot(unit.id,bc.Direction.Southeast); return
-      else: gc.move_robot(unit.id,bc.Direction.Southwest); return
+      else: print("MOVEMENT HEAT", unit.movement_heat());gc.move_robot(unit.id,bc.Direction.Southwest); return
 
 def go_to(unit, dest):  # using bugnav
     # assuming dest is a MapLocation
